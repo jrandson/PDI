590d5e08b6720883053951bf87d75ba6d81d940c
Victor <victorsabino@rocketmail.com>
2016-05-28 01:14:48 -0300

    Finalização

--- Final_work/final_work.cpp
+++ Final_work/final_work.cpp
@@ -10,18 +10,20 @@ using namespace cv;
 using namespace std;
 
 void gerar_valores(int vetor[],int tam, int amostras);
-void menor(int n, int m, float **mat, int *posic_x,int *posic_y);
+void menor(int n, int m, float **mat, int *posic_x,int *posic_y,float *menorzao);
 
 int main(int argc, char** argv){
   Mat image, new_image, sample_image,labels;
-  Vec3b val;
+  Vec3b val,val1;
   int pos_centro_x, pos_centro_y;
-  int n_amostras=200;
+  float minimo;
+  int n_amostras=100;
   int width, height, r_number_x,r_number_y,k;
   srand(time(NULL));
   //Passo 1  --> Carregar imagem colorida
   image = imread(argv[1],CV_LOAD_IMAGE_COLOR);
   image.copyTo(sample_image);
+  image.copyTo(new_image);
   //Amostra da imagem
   Mat m_sampled(n_amostras, n_amostras, CV_8UC3, Scalar(0, 0, 0));
   //Perguntar ao professor como converter essa imagem
@@ -33,6 +35,7 @@ int main(int argc, char** argv){
   val[0] = 0;   //B
   val[1] = 0;   //G
   val[2] = 0; //R
+  val1 = val;
 //Define preto na imagem auxiliar
   for(int i=0;i<width;i++){
     for(int j=0;j<height;j++){
@@ -59,25 +62,38 @@ int main(int argc, char** argv){
 //K-Means
 //Definindo quantidade de clusters onde k define a quantidade de centroides ou clusters
 //Exemplo: se k = 32, Quantidade de centroides = 32*32 = 1024
-k = 32;
+k = 10;
+float teste_x,teste_y,teste_z,erro;
+bool flag=false;
+int azul, verde,vermelho;
+int classificacao[n_amostras][n_amostras];
 float aux;
-float** dist_p_to_c;
-   dist_p_to_c = (float**) malloc (k * sizeof (float *));
-   for (int i = 0; i < k; ++i)
-      dist_p_to_c[i] = (float *) malloc (k * sizeof (float));
+float** dist_p_to_c_local;
+float** dist_p_to_c_global;
+//Inicialização da matriz de centroides
+Mat centers(k, k, CV_8UC3, Scalar(0, 0, 0));
+//Vetor que armazena a posição dos centroides
+int posicao_x[k], posicao_y[k];
+Mat medias = Mat(k,k, CV_64F, double(0));
+int contadores[k][k];
 
-for(int i=0;i<k;i++)
+
+   dist_p_to_c_local = (float**) malloc (k * sizeof (float *));
+   for (int i = 0; i < k; ++i)
+      dist_p_to_c_local[i] = (float *) malloc (k * sizeof (float));
+
+    dist_p_to_c_global = (float**) malloc (n_amostras * sizeof (float *));
+    for (int i = 0; i < n_amostras; ++i)
+      dist_p_to_c_global[i] = (float *) malloc (n_amostras * sizeof (float));
+
+/*for(int i=0;i<k;i++)
 {
     for(int j=0;j<k;j++)
     {
     dist_p_to_c[i][j] = 0.0;
     }
-}
+}*/
 
-//Inicialização da matriz de centroides
-Mat centers(k, k, CV_8UC3, Scalar(0, 0, 0));
-//Vetor que armazena a posição dos centroides
-int posicao_x[k], posicao_y[k];
 
 // gerando posição dos centroides aleatoriamente
 gerar_valores(posicao_x,k,n_amostras);
@@ -92,8 +108,10 @@ for(int i=0;i<k;i++)
          centers.at<Vec3b>(i,j) = m_sampled.at<Vec3b>(posicao_x[i],posicao_y[j]);
     }
 }
-
-list<int> classificacao(n_amostras*n_amostras,0);
+//list<int> classificacao(n_amostras*n_amostras,0);
+int z=0;
+while(z<100){
+flag = true;
  //Primeiros Centroides já selecionados, então o prximo passo é fazer os calculos para classificação das amostras
 for(int i=0;i<n_amostras;i++)
 {
@@ -104,33 +122,167 @@ for(int i=0;i<n_amostras;i++)
         {
             for(int m=0;m<k;m++)
             {
-                val = m_sampled.at<Vec3b>(i,j) - centers.at<Vec3b>(l,m);
-                aux = sqrt(pow((float)val[0],2.0) + pow((float)val[1],2.0) + pow((float)val[2],2.0));
-                dist_p_to_c[l][m] = aux;
+                val = m_sampled.at<Vec3b>(i,j);
+                //cout<<val<<endl;
+                val1 = centers.at<Vec3b>(l,m);
+                //cout<<val1<<endl;
+                azul = (int)val[0] - (int)val1[0];
+                verde = (int)val[1] - (int)val1[1];
+                vermelho = (int)val[2] - (int)val1[2];
+                aux = sqrt(pow((float)azul,2.0) + pow((float)verde,2.0) + pow((float)vermelho,2.0));
+                dist_p_to_c_local[l][m] = aux;
+                dist_p_to_c_global[i][j] = aux;
+                //cout<<aux<<endl;
+                //getchar();
             }
         }
         //Escolha do centroide usando min e
         //Função retorna a posição do centroide de menor distancia;
-        menor(k, k, dist_p_to_c, &pos_centro_x,&pos_centro_y);
+        menor(k, k, dist_p_to_c_local, &pos_centro_x,&pos_centro_y,&minimo);
         //Cada amostra possui uma classificação
         //classificacao dos pontos armazenadas em uma lista com a posicao do centroide codificado em um inteiro
         //r*nc + c;
-        classificacao.push_back(pos_centro_x*k + pos_centro_y);
+        classificacao[i][j] = pos_centro_x*k + pos_centro_y;
     }
 }
-//cout<<classificacao;
-for(list<int>::iterator it=classificacao.begin();it != classificacao.end();++it)
+
+
+for(int g=0;g<k;g++)
+for(int h=0;h<k;h++)
+contadores[g][h]= 0;
+
+//Redefinir os centroides de acordo com a media dos pontos que pertencem a eles;
+for(int g=0;g<k;g++)
 {
-    cout<<" "<<*it<<endl;
+    for(int h=0;h<k;h++)
+    {
+        for(int i=0;i<n_amostras;i++)
+        {
+            for(int j=0;j<n_amostras;j++)
+            {
+                aux = classificacao[i][j];
+                //cout<<"aqui"<<endl;
+                if((g*k + h) == aux)
+                {
+                    medias.at<double>(g,h)= dist_p_to_c_global[i][j]+ medias.at<double>(g,h);
+                    contadores[g][h]+= 1;
+                }
+            }
+        }
+    }
+}//*/
+//CAlculo da média de todos os pontos em cada centroide para selecionar um novo
+for(int i=0;i<k;i++)
+{
+    for(int j=0;j<k;j++)
+    {
+        if(medias.at<double>(i,j) == 0 || contadores[i][j] == 0)
+            continue;
+        else
+            medias.at<double>(i,j) = medias.at<double>(i,j) / contadores[i][j];
+    }
+}
+//cout<<"aqui"<<endl;
+//Encontro de novos RGBs que satisfaçam a equação de distancia entre pontos
+for(int i=0;i<k;i++)
+{
+    for(int j=0;j<k;j++)
+    {
+        while(flag != true)
+        {
+            //cout<<flag<<endl;
+            if(medias.at<double>(i,j) > 255)
+            medias.at<double>(i,j) = 255;
+            //cout<<medias.at<double>(i,j)<<endl;
+            teste_x = rand() %255;
+            teste_y = rand() %255;
+            teste_z = rand() %255;
+            aux = sqrt(pow(teste_x,2.0) + pow(teste_y,2.0) + pow(teste_z,2.0));
+            erro = abs(aux - medias.at<double>(i,j));
+            if(erro <= 200)
+            {
+                //cout<<erro<<endl;
+                val[0] =(int) teste_x;
+                val[1] =(int) teste_y;
+                val[2] =(int) teste_z;
+                //Atualiza novo centroide
+                centers.at<Vec3b>(i,j) = val;
+                flag = true;
+            }
+            else flag = false;
+        }
+    flag = false;
+    }
+}
+z++;
+cout<<z<<endl;
+//Inicia tudo novamente
+}
+int classify[width][height];
+//cout<<centers;
+//Aplicação na imagem
+for(int i=0;i<width;i++)
+{
+    for(int j=0;j<height;j++)
+    {
+    //Acesso aos centroides ( Calculo das distancias de um ponto a todos os centroides );
+        for(int l=0;l<k;l++)
+        {
+            for(int m=0;m<k;m++)
+            {
+            //cout<<"aqui"<<endl;
+              //  getchar();
+                val = new_image.at<Vec3b>(i,j);
+                //cout<<val<<endl;
+                val1 = centers.at<Vec3b>(l,m);
+                //cout<<val1<<endl;
+                azul = (int)val[0] - (int)val1[0];
+                verde = (int)val[1] - (int)val1[1];
+                vermelho = (int)val[2] - (int)val1[2];
+                aux = sqrt(pow((float)azul,2.0) + pow((float)verde,2.0) + pow((float)vermelho,2.0));
+                dist_p_to_c_local[l][m] = aux;
+               // dist_p_to_c_global[i][j] = aux;
+                //cout<<aux<<endl;
+                //getchar();
+            }
+        }
+        //Escolha do centroide usando min e
+        //Função retorna a posição do centroide de menor distancia;
+        menor(k, k, dist_p_to_c_local, &pos_centro_x,&pos_centro_y,&minimo);
+        //Cada amostra possui uma classificação
+        //classificacao dos pontos armazenadas em uma lista com a posicao do centroide codificado em um inteiro
+        //r*nc + c;
+        classify[i][j] = pos_centro_x*k + pos_centro_y;
+    }
 }
 
-/*kmeans(m_sampled, 64, labels,
-            TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0),
-               3, KMEANS_PP_CENTERS, centers);
-*/
+for(int g=0;g<k;g++)
+{
+    for(int h=0;h<k;h++)
+    {
+        for(int i=0;i<width;i++)
+        {
+            for(int j=0;j<height;j++)
+            {
+                aux = classify[i][j];
 
-  //imshow("image_sample",m_sampled);
-free(dist_p_to_c);
+                if((g*k + h) == aux)
+                {
+                    val = centers.at<Vec3b>(g,h);
+                    new_image.at<Vec3b>(i,j) = val;
+
+                }
+            }
+        }
+    }
+}
+
+//cout<<contadores<<endl;
+  imshow("image_sample",image);
+  imshow("image_classify",new_image);
+free(dist_p_to_c_local);
+free(dist_p_to_c_global);
+
   waitKey();
 
   return 0;
@@ -154,7 +306,7 @@ void gerar_valores(int vetor[],int tam, int amostras)
 		}
 	}
 }
-void menor(int n, int m, float **mat, int *posic_x,int *posic_y) {
+void menor(int n, int m, float **mat, int *posic_x,int *posic_y, float *menorzao) {
 
   int i,j;
   float menor, maior;
@@ -173,6 +325,8 @@ void menor(int n, int m, float **mat, int *posic_x,int *posic_y) {
       }
     }
   }
+  *menorzao = menor;
+//cout<<menor<<"< --menor"<<endl;
 
   return;
 }
